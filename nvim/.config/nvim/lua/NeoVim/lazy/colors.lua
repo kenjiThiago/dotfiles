bf85fcd9b1cf2616c92ba5285a3f715479a3d52b
Thiago Kenji <thiago.kenji123@gmail.com>
2024-10-08 14:56:43 -0300

    [nvim] Adicionado novo tema para o nvim.
    Adicionado o tema catppuccin para o nvim e colocado
    ele como tema padrão do nvim.

--- nvim/.config/nvim/lua/NeoVim/lazy/colors.lua
+++ nvim/.config/nvim/lua/NeoVim/lazy/colors.lua
@@ -2,7 +2,7 @@ return {
     {
         'rose-pine/neovim',
         name = 'rose-pine',
-        lazy = false,
+        lazy = true,
         priority = 1000,
         config = function()
             require('rose-pine').setup({
@@ -12,7 +12,19 @@ return {
                     italic = false
                 }
             })
-            vim.cmd.colorscheme("rose-pine")
+            vim.cmd.colorscheme("rose-pine-moon")
         end
     },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true
+            })
+            vim.cmd.colorscheme("catppuccin-macchiato")
+        end
+    }
 }
