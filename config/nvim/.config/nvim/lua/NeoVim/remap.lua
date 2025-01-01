b870224b9efb4fb73035c97a64788db2d6868477
Thiago Kenji <thiago.kenji123@gmail.com>
2025-01-01 04:25:57 -0300

    [nvim] Removido keybind.
    Removido a keybind do TAB e S-TAB, pois
    elas estavam dando conflito com a keybind
    <C-i> para navegar na jumplist do nvim.

--- config/nvim/.config/nvim/lua/NeoVim/remap.lua
+++ config/nvim/.config/nvim/lua/NeoVim/remap.lua
@@ -38,8 +38,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 --Indentação
-vim.keymap.set("n", "<TAB>", ">>")
-vim.keymap.set("n", "<S-TAB>", "<<")
+-- vim.keymap.set("n", "<TAB>", ">>")
+-- vim.keymap.set("n", "<S-TAB>", "<<")
 
 --Tabs
 vim.keymap.set("n", "<leader>to", ":tabnew<CR>")
