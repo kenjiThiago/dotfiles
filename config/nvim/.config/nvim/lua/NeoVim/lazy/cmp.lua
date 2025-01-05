09deed12966779209d9082aaba1c2d9f22293a5c
Thiago Kenji <thiago.kenji123@gmail.com>
2025-01-04 22:42:04 -0300

    [nvim] Adicionado um build no LuaSnip para instalar jsregexp.

--- config/nvim/.config/nvim/lua/NeoVim/lazy/cmp.lua
+++ config/nvim/.config/nvim/lua/NeoVim/lazy/cmp.lua
@@ -1,6 +1,7 @@
 return {
     {
         "L3MON4D3/LuaSnip",
+        build = "make install_jsregexp",
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
         priority = 999,
         dependencies = {
