ab18e71e97667d640baa2b04d01a4220d49c2fa8
Thiago Kenji <thiago.kenji123@gmail.com>
2025-04-02 04:51:28 -0300

    [nvim] Arrumado o virtual_text.

--- config/nvim/.config/nvim/lua/NeoVim/lazy/lsp.lua
+++ config/nvim/.config/nvim/lua/NeoVim/lazy/lsp.lua
@@ -58,6 +58,7 @@ return {
         })
 
         vim.diagnostic.config({
+            virtual_text = true,
             underline = true,
             update_in_insert = false,
             severity_sort = true,
