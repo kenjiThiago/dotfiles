fddc078ba4c7f3a766093618acb049285ed6bf43
Thiago Kenji <thiago.kenji123@gmail.com>
2025-04-21 07:11:13 -0300

    [nvim] Removido o treesitter dos arquivos .csv.

--- config/nvim/.config/nvim/lua/NeoVim/lazy/treesitter.lua
+++ config/nvim/.config/nvim/lua/NeoVim/lazy/treesitter.lua
@@ -14,6 +14,7 @@ return {
                 enable = true,
 
                 additional_vim_regex_highlighting = false,
+                disable = { "csv" }
             },
 
             indent = {
