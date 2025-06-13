7a91caf95c0aa3b88c24f3d5d911e5de1a7f1e80
Thiago Kenji <thiago.kenji123@gmail.com>
2025-06-13 18:30:12 -0300

    [nvim] O cmp mostra a cor da sugestão de cores do tailwind.

--- config/nvim/.config/nvim/lua/NeoVim/lazy/cmp.lua
+++ config/nvim/.config/nvim/lua/NeoVim/lazy/cmp.lua
@@ -57,7 +57,24 @@ return {
                     }
                 ),
                 formatting = {
-                    format = function(_, item)
+                    fields = { "menu", "abbr", "kind" },
+                    format = function(entry, item)
+                        local entryItem = entry:get_completion_item()
+                        local color = entryItem.documentation
+
+                        -- verifica se a cor é hexcolor
+                        if color and type(color) == "string" and color:match "^#%x%x%x%x%x%x$" then
+                            local hl = "hex-" .. color:sub(2)
+
+                            if #vim.api.nvim_get_hl(0, { name = hl }) == 0 then
+                                vim.api.nvim_set_hl(0, hl, { fg = color })
+                            end
+
+                            item.menu = " "
+                            item.menu_hl_group = hl
+                            item.kind_hl_group = hl
+                        end
+
                         local icons = icons.kinds
 
                         if icons[item.kind] then
