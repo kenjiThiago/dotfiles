23e542f3fa7581c270b28211c0d733ec74763be0
Thiago Kenji <thiago.kenji123@gmail.com>
2025-07-03 06:30:16 -0300

    [nvim] Trocado o cmp pelo blink.cmp.

--- config/nvim/.config/nvim/lua/NeoVim/lazy/cmp.lua
+++ config/nvim/.config/nvim/lua/NeoVim/lazy/cmp.lua
@@ -4,113 +4,52 @@ return {
         build = "make install_jsregexp",
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
         dependencies = {
-            "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
         },
         config = function()
             require("luasnip.loaders.from_vscode").lazy_load()
             local ls = require("luasnip")
-            vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-            vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-            vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+            vim.keymap.set({"i"}, "<C-k>", function() ls.expand() end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-l>", function() ls.jump( 1) end, {silent = true})
+            vim.keymap.set({"i", "s"}, "<C-j>", function() ls.jump(-1) end, {silent = true})
         end
     },
 
     {
-        "hrsh7th/nvim-cmp",
+        "saghen/blink.cmp",
+        build = "cargo build --release",
         event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-        },
         config = function()
-            local cmp = require("cmp")
-
-            local cmp_select = { behavior = cmp.SelectBehavior.Insert }
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end,
+            require("blink.cmp").setup({
+                completion = {
+                    menu = { border = "single" },
+                    documentation = {
+                        auto_show = true,
+                        window = { border = "single" }
+                    }
                 },
-                window = {
-                    completion = {
-                        border = "single"
-                    },
-                    documentation = cmp.config.window.bordered(),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-e>'] = cmp.mapping.complete(),
-                }),
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                    { name = 'buffer' },
-                }),
-                formatting = {
-                    fields = { "menu", "abbr", "kind" },
-                    format = function(entry, item)
-                        local entryItem = entry:get_completion_item()
-                        local color = entryItem.documentation
-
-                        -- verifica se a cor é hexcolor
-                        if color and type(color) == "string" and color:match "^#%x%x%x%x%x%x$" then
-                            local hl = "hex-" .. color:sub(2)
-
-                            if #vim.api.nvim_get_hl(0, { name = hl }) == 0 then
-                                vim.api.nvim_set_hl(0, hl, { fg = color })
-                            end
-
-                            item.menu = " "
-                            item.menu_hl_group = hl
-                            item.kind_hl_group = hl
-                        else
-                            item.menu = ""
-                        end
-
-                        local icons = icons.kinds
-
-                        if icons[item.kind] then
-                            item.kind = icons[item.kind] .. item.kind
-                        end
-
-                        local widths = {
-                            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-                            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-                        }
-
-                        for key, width in pairs(widths) do
-                            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-                            end
-                        end
-
-                        return item
-                    end,
-                },
-            })
-
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
+                signature = { window = { border = "single" } },
                 sources = {
-                    { name = 'buffer' }
-                }
-            })
-
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({
-                    { name = 'path' }
-                }, {
-                        { name = 'cmdline' }
-                    }),
-                matching = { disallow_symbol_nonprefix_matching = false }
+                    default = { "lsp", "path", "snippets", "buffer" },
+                    providers = {
+                        buffer = {
+                            opts = {
+                                get_bufnrs = vim.api.nvim_list_bufs,
+                            }
+                        },
+                        lsp = { fallbacks = {} },
+                    }
+                },
+                keymap = {
+                    ["<C-e>"] = { "show", "hide" },
+                    ["<C-l>"] = { "snippet_forward", "fallback" },
+                    ["<C-j>"] = { "snippet_backward", "fallback" },
+                },
+                cmdline = {
+                    keymap = { preset = "inherit" },
+                    completion = { menu = { auto_show = true } },
+                },
+                fuzzy = { implementation = "prefer_rust_with_warning" },
             })
         end
     },
